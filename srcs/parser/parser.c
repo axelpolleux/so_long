9e41a4f874a049ef5fdf0f3eca56a4065c2f051b
apolleux <polleuxaxel@gmail.com>
2026-02-04 17:59:17 +0100

    Used strncmp for parser

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 11:38:20 by apolleux          #+#    #+#             */
-/*   Updated: 2026/02/04 17:21:17 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/02/04 17:56:00 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	main_parser(int argc, char **argv)
 	(void)argv;
 	if (argc != 2)
 		return (error("Only 1 argument\n"));
-	else if (ft_strlen(argv[1]) <= 4 || !ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])))
-		return (error("Only .ber files\n"));
-	else
+	else if (ft_strlen(argv[1]) <= 4 || ft_strncmp(argv[1] + (ft_strlen(argv[1]) - 4), ".ber", 4) != 0)
+	 	return (error("Only .ber files\n"));
+	else if ()
 		printf("Le fichier est prêt a être lu\n");
 	return (1);
 }
