8f46b25d477df15a2ef1c46d4c6f952dfe9b7213
apolleux <polleuxaxel@gmail.com>
2026-02-10 12:36:30 +0100

    feat: check if .ber is empty BREAKING CHANGE: moved width and height variables as pointers instead of return values

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 11:38:20 by apolleux          #+#    #+#             */
-/*   Updated: 2026/02/09 18:48:25 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/02/10 12:20:04 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,35 @@
 #include "libft/libft.h"
 #include "so_long.h"
 
-static int	count_width(char *filepath)
-{
-	int	fd;
-	int	len1;
-	int	len2;
+// static int	check_border(void)
+// {
+// 	return (1);
+// }
 
-	(void)len1;
-	(void)len2;
-	fd = open(filepath, O_RDONLY);
-	// while (get_next_line(fd))
-	// {
-	// 	if
-	// }
-	close(fd);
-	return (1);
-}
-
-int	count_dimension(char *filepath)
+void	get_dimension(char *filepath, int *height, int *width)
 {
-	int	fd;
-	int	len;
+	int		fd;
+	int		height_res;
+	int		width_res;
+	char	*str;
 
 	fd = open(filepath, O_RDONLY);
 	if (fd == -1)
-		return (-1);
-	len = 0;
-	while (get_next_line(fd))
-		len++;
+	{
+		*height = -1;
+		return ;
+	}
+	height_res = 0;
+	width_res = 0;
+	str = "";
+	while (str)
+	{
+		str = get_next_line(fd);
+		height_res++;
+	}
+	*height = height_res - 1;
+	*width = width_res;
 	close(fd);
-	return (len);
 }
 
 static char	**map_maker(char *filepath, int height)
@@ -69,19 +68,15 @@ int	main_parser(int argc, char **argv, char ***map)
 	int	height;
 	int	width;
 
-	height = count_lines(argv[1]);
-	width = count_width(argv[1]);
-	(void)map;
+	get_dimension(argv[1], &height, &width);
 	if (argc != 2)
 		return (error("Only 1 argument\n"));
 	else if (ft_strlen(argv[1]) <= 4 || ft_strncmp(argv[1] + (ft_strlen(argv[1])
 				- 4), ".ber", 4) != 0)
 		return (error("Only .ber files\n"));
-	else if (height == -1)
-		return (error("File doesn't exist, or cannot be opened\n"));
-	else if (width == -1)
-		return (error("Map isn't rectangular\n"));
-	else
-		*map = map_maker(argv[1], height);
+	else if (height <= 0)
+		return (error("File doesn't exist, cannot be opened, or file is empty\n"));
+	*map = map_maker(argv[1], height);
+	printf("Hauteur: %d\nLargeur: %d\n", height, width);
 	return (1);
 }
