de32b5fd559381cda0efde0431b8bd944ff729d0
apolleux <polleuxaxel@gmail.com>
2026-02-13 19:33:07 +0100

    feat: Parser now trolls you with emoticones

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 11:38:20 by apolleux          #+#    #+#             */
-/*   Updated: 2026/02/13 18:57:46 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/02/13 19:29:48 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,7 @@ static int	get_dimension(char *filepath, int *height, int *width)
 
 	fd = open(filepath, O_RDONLY);
 	if (fd == -1)
-	{
-		*height = -1;
-		return (error("Your file doesn't exist, or is empty\nskill issue"));
-	}
+		return (error("File doesn't exist\nskill issue ☉ ‿ ⚆ "));
 	*height = 0;
 	*width = 0;
 	str = get_line(fd);
@@ -44,8 +41,10 @@ static int	get_dimension(char *filepath, int *height, int *width)
 	}
 	free(str);
 	close(fd);
+	if (*height == 0)
+		return (error("(◔_◔) An empty map ?\ninteresting"));
 	if (*height <= 2 || *width <= 2)
-		return (error("I can't breath !!!\nThis map is too small"));
+		return (error("╚(•⌂•)╝I can't breath !!!\nThis map is too small"));
 	return (1);
 }
 
@@ -71,14 +70,14 @@ int	main_parser(int argc, char **argv, char ***map)
 	int	width;
 
 	if (argc > 2)
-		return (error("WOW calm down !!\nI just need one argument"));
+		return (error("ヽ༼ຈʖ̯ຈ༽ﾉ\nWOW calm down !!\nI just need one argument"));
 	else if (argc < 2)
-		return (error("Meh... You don't wanna talk ?\nGive me one argument"));
-	if (!get_dimension(argv[1], &height, &width))
-		return (0);
+		return (error("You don't wanna talk ?\n⊙ω⊙\nGive me one argument"));
 	if (ft_strlen(argv[1]) <= 4 || ft_strncmp(argv[1] + (ft_strlen(argv[1])
 				- 4), ".ber", 4) != 0)
-		return (error("Only .ber files"));
+		return (error("x⸑x how can you miss it ?\nOnly .ber files"));
+	if (!get_dimension(argv[1], &height, &width))
+		return (0);
 	*map = map_maker(argv[1], height);
 	if (!check_border(*map, height, width) || !check_points(*map))
 		return (0);
--- srcs/utils/map_manager.c
+++ srcs/utils/map_manager.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 17:07:11 by apolleux          #+#    #+#             */
-/*   Updated: 2026/02/13 18:58:57 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/02/13 19:31:27 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	check_border(char **map, int height, int width)
 		{
 			while (map[i][++j])
 				if (map[i][j] != '1' && map[i][j] != '\n')
-					return (error("a house without any side walls ? :-|"));
+					return (error("you forgot up and bottom surroundings"));
 		}
 		else
 		{
@@ -33,7 +33,7 @@ int	check_border(char **map, int height, int width)
 			{
 				if ((j == 0 || j == width - 1) && map[i][j] != '\n'
 					&& map[i][j] != '1')
-					return (error("Map must be surrounded in both sides"));
+					return (error("a house without any side walls ? :-|"));
 				j++;
 			}
 		}
@@ -67,10 +67,10 @@ int	counter(char **map, int character)
 int	check_points(char **map)
 {
 	if (counter(map, 'P') != 1)
-		return (error("I've never seen a game without a player"));
+		return (error("I've never seen a game without a player\n(͠≖ ͜ʖ͠≖)"));
 	if (counter(map, 'E') != 1)
-		return (error("Where will you go, without any exit ?"));
+		return (error("Where will you go, without any exit ?\n(ง ͠° ͟ʖ ͡°)ง"));
 	if (counter(map, 'C') < 1)
-		return (error("I'll get bored without any collectibles"));
+		return (error("I'll get bored without any collectibles\n(ಥ﹏ಥ)"));
 	return (1);
 }
